da2242c6d5179c58ea9b184f0c2f5adfe0959f7c
Josh Madakor <josh.madakor@Gmail.com>
2022-07-21 09:55:39 -0700

    Made compatible with Azure Functions

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -5,10 +5,14 @@ import azure.functions as func
 
 
 def main(mytimer: func.TimerRequest) -> None:
+    print("I'm drinking coffee rn.")
+    logging.info("I'm drinking coffee rn.")
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
-    if mytimer.past_due:
-        logging.info('The timer is past due!')
+    #if mytimer.past_due:
+    #    logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+main(None)
